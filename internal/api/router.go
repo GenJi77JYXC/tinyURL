5428371a9be2a70dc01f4383f1cb4f0ff38a74d7
GenJi77JYXC <1612977482@qq.com>
2026-01-16 20:13:40 +0800

    add: 解决跨域问题

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,12 +1,25 @@
 package api
 
 import (
+	"time"
+
 	"github.com/GenJi77JYXC/tinyurl/internal/service"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(svc *service.ShortenerService, authSvc *service.AuthService) *gin.Engine {
 	r := gin.Default()
+
+	// 添加 CORS 中间件（允许本地开发跨域）
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:5174", "https://mahiro.cloud", "https://www.mahiro.cloud"}, // 允许这些源
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Authorization", "Content-Type", "Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	//r.POST("/api/shorten", ShortenHandler(svc))
 	r.GET("/:short", RedirectHandler(svc))
